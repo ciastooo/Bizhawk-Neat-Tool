a763a1c5cd16bb4b5f47bc89e2f6c6d12f285414
Piotr Wontka <leene4@gmail.com>
2019-04-10 22:57:27 +0200

    Init

---
+++ CustomMainForm.cs
@@ -0,0 +1,41 @@
+﻿using BizHawk.Client.ApiHawk;
+using System.Windows.Forms;
+
+[assembly:BizHawkExternalTool("NEAT", "test")]
+namespace BizHawk.Client.EmuHawk
+{
+	public partial class CustomMainForm : Form, IExternalToolForm
+	{
+		public CustomMainForm()
+		{
+			InitializeComponent();
+		}
+
+		public bool UpdateBefore => throw new System.NotImplementedException();
+
+		public bool AskSaveChanges()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void FastUpdate()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void NewUpdate(ToolFormUpdateType type)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Restart()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void UpdateValues()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
