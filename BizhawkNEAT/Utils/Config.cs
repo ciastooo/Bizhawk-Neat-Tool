899d027d0ffde816e0b3c1b03e44625f01329009
Piotr Wontka <leene4@gmail.com>
2019-04-25 20:01:27 +0200

    Refactored structure of repository

--- BizhawkNEAT/Utils/Config.cs
+++ BizhawkNEAT/Utils/Config.cs
@@ -2,17 +2,20 @@
 {
     public static class Config
     {
+        // Species distance coefficients
         public static double C1 { get; private set;}
         public static double C2 { get; private set;}
         public static double C3 { get; private set;}
+        public static int N { get; private set; }
 
         public static double Step { get; private set; }
 
-        public static void Setup(double c1, double c2, double c3, double step)
+        public static void Setup(double c1, double c2, double c3, double n, double step)
         {
             C1 = c1;
             C2 = c2;
             C3 = c3;
+            N = n;
             Step = step;
         }
     }
---
+++ BizhawkNEAT/Utils/Extensions.cs
@@ -0,0 +1,87 @@
+﻿using BizhawkNEAT.Neat;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BizhawkNEAT.Utils
+{
+    public static class Extensions
+    {
+        public static T GetRandomElement<T>(this IEnumerable<KeyValuePair<int, T>> source)
+        {
+            var randomIndex = RandomGenerator.GetRandom().Next(source.Count());
+            return source.First(x => x.Key == randomIndex).Value;
+        }
+
+        public static ConnectionGene GetConnection(this IDictionary<int, ConnectionGene> sourceDictionary, NodeGene source, NodeGene target)
+        {
+            return sourceDictionary.Where(x =>
+                (x.Value.PreviousNode == source && x.Value.NextNode == target) ||
+                (x.Value.PreviousNode == target && x.Value.NextNode == source))
+                .Select(x => x.Value).FirstOrDefault();
+        }
+
+        public static double GetSpecieDistance(this IDictionary<int, ConnectionGene> sourceDictionary, IDictionary<int, ConnectionGene> targetDictionary)
+        {
+            var geneCount = 0;
+            var disjointGenes = 0;
+            var excessGenes = 0;
+            var weightDifference = 0d;
+
+
+            var targetMaxId = 0;
+            var targetGeneCount = 0;
+            foreach (var targetGeneKey in targetDictionary.Keys)
+            {
+                targetGeneCount++;
+                if (targetGeneKey > targetMaxId)
+                {
+                    targetMaxId = targetGeneKey;
+                };
+            }
+
+            var sourceMaxId = 0;
+            foreach (var sourceGeneKey in sourceDictionary.Keys)
+            {
+                geneCount++;
+
+                if (sourceGeneKey > sourceMaxId)
+                {
+                    sourceMaxId = sourceGeneKey;
+                };
+
+                if (!targetDictionary.ContainsKey(sourceGeneKey))
+                {
+                    if(sourceGeneKey < targetMaxId)
+                    {
+                        disjointGenes++;
+                    } else
+                    {
+                        excessGenes++;
+                    }
+                }
+            }
+
+            foreach (var targetGeneKey in targetDictionary.Keys)
+            {
+                if (!sourceDictionary.ContainsKey(targetGeneKey))
+                {
+                    if (targetGeneKey < sourceMaxId)
+                    {
+                        disjointGenes++;
+                    }
+                    else
+                    {
+                        excessGenes++;
+                    }
+                }
+            }
+
+            if (targetGeneCount > geneCount)
+            {
+                geneCount = targetGeneCount;
+            }
+
+            return ((Config.C1 * excessGenes) + (Config.C2 * disjointGenes)) / geneCount + Config.C3 * weightDifference;
+        }
+    }
+}
